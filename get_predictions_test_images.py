374f9d5e9b1e459a99311d5705b66b13f0ef5229
czarina <zou.christina@gmail.com>
2017-06-10 19:49:48 -0700

    predictions script

---
+++ get_predictions_test_images.py
@@ -0,0 +1,50 @@
+import os, sys
+import argparse
+
+import tensorflow as tf
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+def eval_images(images_path):
+    
+
+    # Loads label file, strips off carriage return
+    label_lines = [line.rstrip() for line 
+                       in tf.gfile.GFile("species_model/retrained_labels.txt")]
+
+    # Unpersists graph from file
+    with tf.gfile.FastGFile("species_model/retrained_graph.pb", 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name='')
+
+    # write predictions to file
+    outfile = open("species_model/test_predictions.txt", "w")
+
+    with tf.Session() as sess:
+        for image_path in images_path:
+            # Read in the image_data
+            image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+            # Feed the image_data as input to the graph and get first prediction
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+            
+            predictions = sess.run(softmax_tensor, \
+                     {'DecodeJpeg/contents:0': image_data})
+            
+            # Sort to show labels of first prediction in order of confidence
+            top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+            
+            # Print top 5 predictions per image
+            results = []
+            for node_id in top_k[0:5]:
+                human_string = label_lines[node_id]
+                score = predictions[0][node_id]
+                results.append(human_string + "," + str(round(score,4)))
+            outfile.write(image_path + '\t' + '\t'.join(results) + '\n')
+            print(image_path + '\t' + '\t'.join(results) + '\n')
+
+# change this as you see fit
+images_path = sys.argv[1]
+f = open(images_path, "r")
+images = f.read().split('\n')
+eval_images(images)
